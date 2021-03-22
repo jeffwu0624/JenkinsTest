43d186308fb30e7f1856f143f74038624bc09571
JeffWu0624 <jeffwu0624@hotmail.com>
2021-03-22 21:57:56 +0800

    add test project

---
+++ JenkinsTest/JenkinsTest.UnitTest/CalculatorTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace JenkinsTest.UnitTest
+{
+    public class CalculatorTest
+    {
+        private Calculator _calculator = new Calculator();
+
+        [Test]
+        public void Add_positive_integer()
+        {
+            SumShouldBe(1, 2, 3);
+        }
+
+        private void SumShouldBe(int a, int b, int expected)
+        {
+            Assert.AreEqual(expected, _calculator.Add(a, b));
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
